43fd7c4e88c55e061e8b50920b03a682d93a4719
kaisuzuk <szkakg@gmail.com>
2026-04-05 12:59:06 +0900

    mariadb

---
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d "/var/lib/mysql/$MYSQL_NAME" ]; then
+    "$@" --skip-networking &
+    MARIADB_PID=$!
+
+    until mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    mariadb -u root <<EOF
+CREATE DATABASE $MYSQL_NAME;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    kill $MARIADB_PID
+    wait $MARIADB_PID
+
+fi
+
+exec "$@"
