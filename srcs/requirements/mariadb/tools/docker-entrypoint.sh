7e6cbed6526d2b8636481e4e7b7781d9a7eec5b9
kaisuzuk <szkakg@gmail.com>
2026-04-05 14:23:01 +0900

    bind-address

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
+if [ -z "$MYSQL_NAME" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    echo "[Entrypoint] Error: Required environment variables are not set"
+    exit 1
+fi
+
 if [ ! -d "/var/lib/mysql/$MYSQL_NAME" ]; then
     "$@" --skip-networking &
     MARIADB_PID=$!
@@ -12,12 +17,12 @@ if [ ! -d "/var/lib/mysql/$MYSQL_NAME" ]; then
         sleep 1
     done
 
-    mariadb -u root <<EOF
-CREATE DATABASE $MYSQL_NAME;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
+    mariadb -u root <<-EOF
+        CREATE DATABASE $MYSQL_NAME;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+        CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+        GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%';
+        FLUSH PRIVILEGES;
 EOF
 
     kill $MARIADB_PID
